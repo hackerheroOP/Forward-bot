dc738fbbc59924aadd0711838ea405c70898caa4
hackerheroOP <78025240+hackerheroOP@users.noreply.github.com>
2025-06-22 01:12:21 +0530

    Update channel_bot.py

--- channel_bot.py
+++ channel_bot.py
@@ -1,22 +1,4 @@
 
-from flask import Flask, jsonify
-import threading
-
-# Initialize Flask app
-app = Flask(__name__)
-
-# Define the root endpoint
-@app.route("/")
-def home():
-    return jsonify({"message": "Welcome To PandazNetwork"})
-
-# Function to run the Flask app
-def run_flask():
-    app.run(host="0.0.0.0", port=8080)
-
-# Run Flask in a separate thread
-flask_thread = threading.Thread(target=run_flask)
-flask_thread.start()
 
 
 
@@ -180,3 +162,22 @@ class ChannelCopyBot:
 
     def run(self):
         self.bot.run()
+
+from flask import Flask, jsonify
+import threading
+
+# Initialize Flask app
+app = Flask(__name__)
+
+# Define the root endpoint
+@app.route("/")
+def home():
+    return jsonify({"message": "Welcome To PandazNetwork"})
+
+# Function to run the Flask app
+def run_flask():
+    app.run(host="0.0.0.0", port=8080)
+
+# Run Flask in a separate thread
+flask_thread = threading.Thread(target=run_flask)
+flask_thread.start()
